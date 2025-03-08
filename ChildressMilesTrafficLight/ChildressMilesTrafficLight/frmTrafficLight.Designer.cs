db9f216c8fe75fcf48b97ab1f2699ddf5ae47ccd
mileschildress88 <miles.childress88@gmail.com>
2025-03-07 21:41:30 -0600

    Add files via upload

---
+++ ChildressMilesTrafficLight/ChildressMilesTrafficLight/frmTrafficLight.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace ChildressMilesTrafficLight
+{
+    partial class frmTrafficLight
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTNExit = new System.Windows.Forms.Button();
+            this.TXTRedLight = new System.Windows.Forms.TextBox();
+            this.TXTYellowLight = new System.Windows.Forms.TextBox();
+            this.TXTGreenLight = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BTNExit
+            // 
+            this.BTNExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.BTNExit.Location = new System.Drawing.Point(115, 90);
+            this.BTNExit.Name = "BTNExit";
+            this.BTNExit.Size = new System.Drawing.Size(73, 31);
+            this.BTNExit.TabIndex = 3;
+            this.BTNExit.Text = "E&xit";
+            this.BTNExit.UseVisualStyleBackColor = true;
+            this.BTNExit.Click += new System.EventHandler(this.BTNExit_Click);
+            // 
+            // TXTRedLight
+            // 
+            this.TXTRedLight.Location = new System.Drawing.Point(101, 12);
+            this.TXTRedLight.Name = "TXTRedLight";
+            this.TXTRedLight.ReadOnly = true;
+            this.TXTRedLight.Size = new System.Drawing.Size(100, 20);
+            this.TXTRedLight.TabIndex = 4;
+            // 
+            // TXTYellowLight
+            // 
+            this.TXTYellowLight.Location = new System.Drawing.Point(101, 38);
+            this.TXTYellowLight.Name = "TXTYellowLight";
+            this.TXTYellowLight.ReadOnly = true;
+            this.TXTYellowLight.Size = new System.Drawing.Size(100, 20);
+            this.TXTYellowLight.TabIndex = 5;
+            // 
+            // TXTGreenLight
+            // 
+            this.TXTGreenLight.Location = new System.Drawing.Point(101, 64);
+            this.TXTGreenLight.Name = "TXTGreenLight";
+            this.TXTGreenLight.ReadOnly = true;
+            this.TXTGreenLight.Size = new System.Drawing.Size(100, 20);
+            this.TXTGreenLight.TabIndex = 6;
+            // 
+            // frmTrafficLight
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.BTNExit;
+            this.ClientSize = new System.Drawing.Size(295, 224);
+            this.Controls.Add(this.TXTGreenLight);
+            this.Controls.Add(this.TXTYellowLight);
+            this.Controls.Add(this.TXTRedLight);
+            this.Controls.Add(this.BTNExit);
+            this.Name = "frmTrafficLight";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TrafficLight";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button BTNExit;
+        private System.Windows.Forms.TextBox TXTRedLight;
+        private System.Windows.Forms.TextBox TXTYellowLight;
+        private System.Windows.Forms.TextBox TXTGreenLight;
+    }
+}
+
---
+++ ChildressMilesTrafficLight/ChildressMilesTrafficLight/frmTrafficLight.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//AUTHOR: Miles Childress
+//COURSE: ISTM 250.501
+//FORM: frmTrafficLight
+//PURPOSE: This program is designed to take the users input from clicks and the "tab" key to change the color of the buttons on the "traffic light"
+//INPUT: The input for this program is from the "tab" key and the user's clicks
+//PROCESS: This program changes the buttons BackColor whenever the user inputs the "tab" key or clicks on one of the buttons
+//OUTPUT: The output from this program is just the color changing on the buttons of the "traffic light" to their associated color.
+//HONOR CODE: “On my honor, as an Aggie, I have neither given
+// nor received unauthorized aid on this academic
+// work." Signature: Miles Childress
+
+
+
+namespace ChildressMilesTrafficLight
+{
+    public partial class frmTrafficLight : Form
+    {
+        public frmTrafficLight()
+        {
+            InitializeComponent();
+            //Initially sets the color of TXTGreenLight to green
+            TXTGreenLight.BackColor = Color.Green;
+            //If the textbox has focus, then the correlating callback to the event is called
+            TXTRedLight.GotFocus += TXTRedLight_GotFocus;
+            TXTGreenLight.GotFocus += TXTGreenLight_GotFocus;
+            TXTYellowLight.GotFocus += TXTYellowLight_GotFocus;
+          //  TXTRedLight.Click += TXTRedLight_GotFocus;
+           // TXTYellowLight.Click += TXTYellowLight_GotFocus;
+           // TXTGreenLight.Click += TXTGreenLight_GotFocus;
+
+            //sets BTNGreenLight color to green initially and the BTNRedLight and BTN YellowLight to Dark Grey
+           // BTNGreenLight.BackColor = Color.Green;
+           // BTNRedLight.BackColor = Color.DarkGray;
+           // BTNYellowLight.BackColor = Color.DarkGray;
+
+            //this was my solution to the changing colors as if the button is in focus then it changes the colors of the buttons accordingly 
+          //  if (BTNGreenLight.Focus())
+         //   {
+           //     BTNGreenLight.BackColor= Color.Green;
+          //      BTNRedLight.BackColor = Color.DarkGray;
+           //     BTNYellowLight.BackColor = Color.DarkGray;
+          //  }
+          //  if (BTNYellowLight.Focus())
+          //  {
+           //     BTNGreenLight.BackColor = Color.DarkGray;
+            //    BTNRedLight.BackColor = Color.DarkGray;
+             //   BTNYellowLight.BackColor = Color.Yellow;
+//          }
+          //  if(BTNRedLight.Focus())
+          //  {
+            //    BTNGreenLight.BackColor = Color.DarkGray;
+           //     BTNRedLight.BackColor = Color.Red;
+            //    BTNYellowLight.BackColor = Color.DarkGray;
+          //  }
+      //  }
+
+
+        //Below is code trying to change the colors with the event of a tab key pressed but I can use the .focus with an if statement to solve this issue *************
+       // void BTNRedLight_Enter(object sender, EventArgs e)
+      //  {
+       //     BTNRedLight.BackColor = Color.Red;
+      //      BTNGreenLight.BackColor = Color.DarkGray;
+       //     BTNYellowLight.BackColor = Color.DarkGray;
+      //  }
+
+      //  void BTNYellowLight_Enter(object sender, EventArgs e)
+      //  {
+       //     BTNRedLight.BackColor = Color.DarkGray;
+        //    BTNGreenLight.BackColor = Color.DarkGray;
+        //    BTNYellowLight.BackColor = Color.DarkGray;
+      //  }
+
+       // void BTNGreenLight_Enter(object sender, EventArgs e)
+       // {
+         //   BTNRedLight.BackColor = Color.DarkGray;
+         //   BTNGreenLight.BackColor = Color.Green;
+          //  BTNYellowLight.BackColor = Color.DarkGray;
+        }
+
+      //  private void TXTYellowLight_Click(object sender, EventArgs e)
+       // {
+       //     throw new NotImplementedException();
+      //  }
+
+        private void BTNExit_Click(object sender, EventArgs e)
+        {
+            //closes the program when the exit button is clicked
+            Close();
+        }
+
+        private void TXTRedLight_GotFocus(object sender, EventArgs e)
+        {
+            //If the TXTRedLight has focus, then it sets it color to red and the other text boxes to dark gray
+            TXTRedLight.BackColor = Color.Red;
+            TXTGreenLight.BackColor = Color.DarkGray;
+            TXTYellowLight.BackColor = Color.DarkGray;
+        }
+
+        private void TXTYellowLight_GotFocus(Object sender, EventArgs e)
+        {
+            //If the TXTYellowLight has focus, then it sets the color to yellow and the other text boxes to dark gray
+            TXTRedLight.BackColor = Color.DarkGray;
+            TXTGreenLight.BackColor = Color.DarkGray;
+            TXTYellowLight.BackColor = Color.Yellow;
+        }
+
+        private void TXTGreenLight_GotFocus(Object sender, EventArgs e)
+        {
+            //If the TXTGreenLight has focus, then it sets the color to green and the other text boxes to dark gray
+            TXTRedLight.BackColor = Color.DarkGray;
+            TXTGreenLight.BackColor = Color.Green;
+            TXTYellowLight.BackColor = Color.DarkGray;
+        }
+
+      //  private void TXTRedLight_Click(object sender, EventArgs e)
+      //  {
+       //     TXTRedLight_GotFocus();
+      //  }
+    }
+}
